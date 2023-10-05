ca93e09cc3964b8c6a1ba98e09667527e156d30d
Yoo Jia Ler <67042654+ninjachicken100@users.noreply.github.com>
2023-10-05 15:40:54 +0800

    Updated code files and requirements.txt
    requirements.txt downloads most libraries necessary

--- tempSensor.py
+++ tempSensor.py
@@ -1,24 +1,24 @@
 import Adafruit_DHT
 import time
-import pymongo 
+import pymongo
+from datetime import datetime
 from flask import Flask 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-
-uri = "mongodb+srv://CPSProject:<password>@cluster0.ybbw04x.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://CPSProject:CPSProject@cluster0.ybbw04x.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['Project']
-collection = db["Thermal"]
+collection = db["Temperature"]
 
 DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 4
+DHT_PIN = 27
 
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
         print("Temp={0:01f}C Humidity={1:0.1f}%".format(temperature,humidity))
-        collection.insert_one({'temperature': temperature, 'humidity': humidity})
+        collection.insert_one({"datetime": datetime.now(),"temperature": temperature, "humidity": humidity})
     else:
-        print("Sensor failure. Check wiring.");
-    time.sleep(3)
+        print("Sensor failure. Check wiring.")
+    time.sleep(2)

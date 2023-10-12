d72aa36b53d39abbcb80c4b67bbf4709524a28f2
Seekay <gohcheekiat@gmail.com>
2023-10-12 17:11:43 +0800

    updates for sound channel just gets newest entries

--- telegram_bot/sound_channel.py
+++ telegram_bot/sound_channel.py
@@ -17,39 +17,47 @@ except Exception as e:
     print(e)
 db = client.Project
 sound_db = db["Sound"].find()
+collection = db["Sound"]
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 channel_id = '-1001862115225'
-sound_level = 31
+sound_level = 10
 latitude = 1.29845948398373  # Hardcoded location coordinates
 longitude = 103.85161150371341
 location_name = ''
+loc_dict = {location_name: [latitude, longitude]}
 threshold = 50  # percentage
 count_events = 0
 
-for pair in sound_db:
-    sound_agg = (
-        sum(1 for value in pair["sound"] if value > sound_level) / len(pair["sound"])) * 100
-    if sound_agg > threshold:
-        count_events += 1
-    else:
-        count_events = 0  # reset events
+change_stream = collection.watch()
 
-    if count_events == 1:
-        # Get the location name based on latitude and longitude
-        location = geolocator.reverse((latitude, longitude), exactly_one=True)
-        if location:
-            location_name = location.address
-            bot.send_message(chat_id=channel_id, text=f"Current sound level is over {sound_level}dB for 30 seconds at the following location: {location_name}")
-            # bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
+for change in change_stream:
+    print(change)
+    if 'fullDocument' in change and 'Sound' in change['fullDocument']:
+        #Sound_level = change['fullDocument']['Sound']
+        sound_agg = (
+            sum(1 for value in change['fullDocument']['Sound'] if value > sound_level) / len(change['fullDocument']['Sound'])) * 100
+        if sound_agg > threshold:
+            count_events += 1
+        else:
+            count_events = 0  # reset events
 
-    elif count_events == 10:
-        # Get the location name based on latitude and longitude
-        location = geolocator.reverse((latitude, longitude), exactly_one=True)
-        if location:
-            location_name = location.address
-            bot.send_message(chat_id=channel_id, text=f"Reminder to check on high sound levels prolonging 1 min 30 seconds at the following location: {location_name}")
-            # bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
+        if count_events == 1:
+            # Get the location name based on latitude and longitude
+            location = geolocator.reverse((loc_dict[location_name][0], loc_dict[location_name][1]), exactly_one=True)
+            if location:
+                location_name = location.address
+                bot.send_message(chat_id=channel_id, text=f"Current sound level is over {sound_level}dB for 30 seconds at the following location: {location_name}")
+                bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
+
+        elif count_events == 10 :
+            # Get the location name based on latitude and longitude
+            location = geolocator.reverse((loc_dict[location_name][0], loc_dict[location_name][1]), exactly_one=True)
+            if location:
+                location_name = location.address
+                bot.send_message(chat_id=channel_id, text=f"Reminder to check on high sound levels prolonging 1 min 30 seconds at the following location: {location_name}")
+                # bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
+
 
 

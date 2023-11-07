412e65a4023ddc0ef451f2bedf5bddd18a2adb5f
Seekay <gohcheekiat@gmail.com>
2023-11-07 18:00:58 +0800

    1030 pm threshold noise notification

--- telegramBot/noiseNotifications.py
+++ telegramBot/noiseNotifications.py
@@ -47,37 +47,41 @@ change_stream = collection.watch()
 # bot.send_message(chat_id=channel_id, text=f"Detected sound levels over {sound_level}dB at the following location: {location}")
 # bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
 
-for change in change_stream:
-    threshold_count = 0.6 * len(change['fullDocument']['Sound'])
-    if 'fullDocument' in change and 'Sound' in change['fullDocument']:
-        # obtain all 60 sound values recorded over the course of 5 minutes and checks if it exceeds threshold
-        exceed_threshold_count = 0
-        for sound_level in change['fullDocument']['Sound']:
-            if sound_level > sound_threshold:
-                exceed_threshold_count += 1
-        
-        if exceed_threshold_count > threshold_count: # if more than 60% of the sound values of one entry is exceeded threshold
-            continuous_event_counter +=1
-            location = geolocator.reverse((loc_dict[location_name][0], loc_dict[location_name][1]), exactly_one=True) # Get the location name based on latitude and longitude
-            location_name = location.address
-            if continuous_event_counter == 1:
-                bot.send_message(chat_id=channel_id, text=f"Detected sound levels over {sound_level}dB \nat the following location: {location_name}")
-                bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
-                lastsent_notification_time = datetime.datetime.now()
+# Define the time threshold as 10:30 PM
+notification_time = datetime.datetime.now().replace(hour=22, minute=30, second=0, microsecond=0)
 
-            elif continuous_event_counter <= 3  and (datetime.datetime.now() - lastsent_notification_time).total_seconds() > 600: #send second continuous event at after 15min (20 min elapsed)
-                bot.send_message(chat_id=channel_id, text=f"Detected sound levels over {sound_level}dB \nat the following location: {location_name}")
-                bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
-                lastsent_notification_time = datetime.datetime.now()
+if datetime.datetime.now() >= notification_time:
+    for change in change_stream:
+        threshold_count = 0.6 * len(change['fullDocument']['Sound'])
+        if 'fullDocument' in change and 'Sound' in change['fullDocument']:
+            # obtain all 60 sound values recorded over the course of 5 minutes and checks if it exceeds threshold
+            exceed_threshold_count = 0
+            for sound_level in change['fullDocument']['Sound']:
+                if sound_level > sound_threshold:
+                    exceed_threshold_count += 1
             
-            elif continuous_event_counter <= 6 and (datetime.datetime.now() - lastsent_notification_time).total_seconds() > 900:
-                bot.send_message(chat_id=channel_id, text=f"Detected sound levels over {sound_level}dB \nat the following location: {location_name}")
-                bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
-                lastsent_notification_time = datetime.datetime.now()
-            
-            elif continuous_event_counter > 6 and (datetime.datetime.now() - lastsent_notification_time).total_seconds() > 1800:
-                bot.send_message(chat_id=channel_id, text=f"Detected sound levels over {sound_level}dB \nat the following location: {location_name}")
-                bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
-                lastsent_notification_time = datetime.datetime.now()
-        else:
-            continuous_event_counter == 0
+            if exceed_threshold_count > threshold_count: # if more than 60% of the sound values of one entry is exceeded threshold
+                continuous_event_counter +=1
+                location = geolocator.reverse((loc_dict[location_name][0], loc_dict[location_name][1]), exactly_one=True) # Get the location name based on latitude and longitude
+                location_name = location.address
+                if continuous_event_counter == 1:
+                    bot.send_message(chat_id=channel_id, text=f"Detected sound levels over {sound_level}dB \nat the following location: {location_name}")
+                    bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
+                    lastsent_notification_time = datetime.datetime.now()
+
+                elif continuous_event_counter <= 3  and (datetime.datetime.now() - lastsent_notification_time).total_seconds() > 600: #send second continuous event at after 15min (20 min elapsed)
+                    bot.send_message(chat_id=channel_id, text=f"Detected sound levels over {sound_level}dB \nat the following location: {location_name}")
+                    bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
+                    lastsent_notification_time = datetime.datetime.now()
+                
+                elif continuous_event_counter <= 6 and (datetime.datetime.now() - lastsent_notification_time).total_seconds() > 900:
+                    bot.send_message(chat_id=channel_id, text=f"Detected sound levels over {sound_level}dB \nat the following location: {location_name}")
+                    bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
+                    lastsent_notification_time = datetime.datetime.now()
+                
+                elif continuous_event_counter > 6 and (datetime.datetime.now() - lastsent_notification_time).total_seconds() > 1800:
+                    bot.send_message(chat_id=channel_id, text=f"Detected sound levels over {sound_level}dB \nat the following location: {location_name}")
+                    bot.send_location(chat_id=channel_id,latitude=latitude, longitude=longitude)
+                    lastsent_notification_time = datetime.datetime.now()
+            else:
+                continuous_event_counter == 0
